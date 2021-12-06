b293baad72f9842ff085ab154abbccd587371747
vamsikrish-A <krishnavamsi165@gmail.com>
2021-12-06 22:16:36 +0530

    Added: packages and initial project

---
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollServiceApplication.class, args);
+	}
+
+}
