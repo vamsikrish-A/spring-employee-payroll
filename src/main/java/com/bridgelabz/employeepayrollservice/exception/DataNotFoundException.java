9ff50a3d5e4eca2c76f25bbac5a41b780a57b750
vamsikrish-A <krishnavamsi165@gmail.com>
2021-12-15 22:34:58 +0530

    Added:custom and global exception class

---
+++ src/main/java/com/bridgelabz/employeepayrollservice/exception/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeepayrollservice.exception;
+
+public class DataNotFoundException extends CustomException {
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/bridgelabz/employeepayrollservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.employeepayrollservice.exception;
+
+import com.bridgelabz.employeepayrollservice.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ResponseDto> handleCustomException(CustomException exception) {
+        ResponseDto responseDto = new ResponseDto(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<ResponseDto>(responseDto, (HttpStatus) responseDto.getData());
+    }
+
+    @ExceptionHandler(DataNotFoundException.class)
+    public ResponseEntity<ResponseDto> handleNotFoundException(DataNotFoundException exception) {
+        ResponseDto responseDto = new ResponseDto(exception.getMessage(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity(responseDto, (HttpStatus) responseDto.getData());
+    }
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ResponseDto> handleBadRequestExceptionException(BadRequestException exception) {
+        ResponseDto responseDto = new ResponseDto(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity(responseDto, (HttpStatus) responseDto.getData());
+    }
+
+
+}
