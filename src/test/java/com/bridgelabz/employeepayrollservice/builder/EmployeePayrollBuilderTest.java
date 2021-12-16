52f6a2514bd83add6ade61015720596fba41d9ab
vamsikrish-A <krishnavamsi165@gmail.com>
2021-12-17 01:38:45 +0530

    Added:testcases for builderEntity and Dto

---
+++ src/test/java/com/bridgelabz/employeepayrollservice/builder/EmployeePayrollBuilderTest.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.employeepayrollservice.builder;
+
+import com.bridgelabz.employeepayrollservice.dto.EmployeePayrollDto;
+import com.bridgelabz.employeepayrollservice.entity.EmployeePayrollData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeePayrollBuilderTest {
+    @InjectMocks
+    private EmployeePayrollBuilder employeePayrollBuilder;
+
+    @Test
+    void buildEntityTest() {
+        EmployeePayrollDto employeePayrollDto = new EmployeePayrollDto();
+        employeePayrollDto.setName("Vamsi");
+        employeePayrollDto.setDept("Production");
+        employeePayrollDto.setSalary(30000);
+        EmployeePayrollData employeePayrollData = new EmployeePayrollData();
+        employeePayrollData = employeePayrollBuilder.buildEmployeePayrollEntity(employeePayrollDto, employeePayrollData);
+        Assertions.assertEquals(employeePayrollDto.getName(), employeePayrollData.getName());
+        Assertions.assertEquals(employeePayrollDto.getDept(), employeePayrollData.getDept());
+        Assertions.assertEquals(employeePayrollDto.getSalary(), employeePayrollData.getSalary());
+
+    }
+}
